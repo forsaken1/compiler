da6dae83590e24bfc6348b1863d91e3ed4f9d403
Alexey Krylov <alexey2142@gmail.com>
2012-12-18 07:34:38 +1100

    Переделал DrawPath

--- node.h
+++ node.h
@@ -8,9 +8,11 @@ protected:
 	void DrawPath(int i, bool b) {
 		string str = b ? "|   " : "    ";
 		
-		for(int j = 0; j < i; j++) cout << str;
+		for(int j = 0; j < i - 1; j++) cout << "    ";
+		for(int j = i <= 0 ? 0 : i - 1; j < i; j++) cout << str;
 		cout << "|" << endl;
-		for(int j = 0; j < i; j++) cout << str;
+		for(int j = 0; j < i - 1; j++) cout << "    ";
+		for(int j = i <= 0 ? 0 : i - 1; j < i; j++) cout << str;
 		cout << "+---";
 	}
 
